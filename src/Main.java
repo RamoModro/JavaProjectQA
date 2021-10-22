f16e4853a8e486cf63d7921bc92ad32a49e89820
RamoModro <ramona.modrogan@gmail.com>
2021-10-22 11:08:21 +0300

    tema introducere in java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello there world");
+        System.out.println("Hello\nRamona");
+
+        System.out.println("Rezultatul sumei:" + (237+22));
+
+        int a=11;
+        double b=13;
+        System.out.println("Rezultatul impartirii:" + (a/b));
+
+        System.out.println("a."+ (-5 + 8 * 6));
+        System.out.println("b." + ((55+9) % 9));
+        System.out.println("c."+ (20 + -3*5 / 8));
+        System.out.println("d." + (5 + 15 / 3 * 2 - 8 % 3 ));
 
 
 
