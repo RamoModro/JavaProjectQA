0d4c26435baaa7a9e31b7b92f370c6b93e20ddb9
RamoModro <ramona.modrogan@gmail.com>
2021-10-25 21:05:45 +0300

    Hello

---
+++ src/Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator {
+
+    public static double sum(int first, double second){
+        double result = first+second;
+        return result;
+    }
+
+    public static double substraction(int first, double second){
+        double result = first - second;
+        return result;
+    }
+
+    public static double multiply(int first, double second){
+        double result = first * second;
+        return result;
+    }
+
+    public static double divide(int first, double second){
+        double result = first / second;
+        return result;
+    }
+
+    public static double modulo(int first, double second){
+        double result = first % second;
+        return result;
+    }
+}
--- src/Lab2.java
+++ src/Lab2.java
@@ -3,41 +3,23 @@ public class Lab2 {
     public static int y = 3;
 
     public static void main(String[] args) {
-        double sumresult = sum(x,y);
-        double substractresult = substraction(x,y);
-        double multiplyresult = multiply(x,y);
-        double divisionresult = divide(x,y);
-        double moduloresult = modulo(x,y);
-        System.out.println(sumresult);
-        System.out.println(substractresult);
-        System.out.println(multiplyresult);
-        System.out.println(divisionresult);
-        System.out.println(moduloresult);
+        Calculator calc = new Calculator();
+        Print printMyName = new Print();
+
+        double sumresult = calc.sum(x,y);
+        double substractresult = calc.substraction(x,y);
+        double multiplyresult = calc.multiply(x,y);
+        double divisionresult = calc.divide(x,y);
+        double moduloresult = calc.modulo(x,y);
+        //System.out.println(sumresult);
+        //System.out.println(substractresult);
+       // System.out.println(multiplyresult);
+        //System.out.println(divisionresult);
+        //System.out.println(moduloresult);
+
+        System.out.println("Hello \nRamona");
 ;    }
 
-    public static double sum(int first, double second){
-        double result = first+second;
-        return result;
-    }
 
-    public static double substraction(int first, double second){
-        double result = first - second;
-        return result;
-    }
-
-   public static double multiply(int first, double second){
-        double result = first * second;
-        return result;
-   }
-
-   public static double divide(int first, double second){
-        double result = first / second;
-        return result;
-   }
-
-   public static double modulo(int first, double second){
-        double result = first % second;
-        return result;
-   }
 
 }
